2d9696b860dab02cb1242ee1e005a138284c23f9
Android Studio <devandroid@ort>
2021-08-10 10:26:23 -0300

    .

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,6 @@ class Form extends Component {
             name="mascota"
             class="u-full-width"
             placeholder="Nombre Mascota"
-            value=""
           ></input>
           <label>Nombre Dueño</label>
           <input
@@ -20,12 +19,11 @@ class Form extends Component {
             name="propietario"
             class="u-full-width"
             placeholder="Nombre dueño de la mascota"
-            value=""
           ></input>
           <label>Fecha</label>
-          <input type="date" name="fecha" class="u-full-width" value=""></input>
+          <input type="date" name="fecha" class="u-full-width"></input>
           <label>hora</label>
-          <input type="time" name="hora" class="u-full-width" value=""></input>
+          <input type="time" name="hora" class="u-full-width"></input>
           <label>Sintomas</label>
           <textarea name="sintomas" class="u-full-width"></textarea>
           <button type="submit" class="u-full-width button-primary">
